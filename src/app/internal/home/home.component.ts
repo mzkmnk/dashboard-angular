a5ad8b6c4151d460b0fb2109267e5cb6e13c2e0e
mzkmnk <sma0729mex@gmail.com>
2024-09-15 15:43:26 +0900

    feat:UI作成

--- src/app/internal/home/home.component.ts
+++ src/app/internal/home/home.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+
+export type AvatarInterface = {
+  path  : string,
+  label : string,
+  size  : 'large' | 'normal' | 'xlarge' | undefined,
+  shape : 'square' | 'circle' | undefined,
+  style : {
+    [key in string]:string
+  } 
+}
+
+export type TaskStatus = 'Ready' | 'On Progress' | 'Needs Review' | 'Done';
 
 @Component({
-  selector    : 'app-home',
-  standalone  : true,
-  imports     : [ ButtonModule ],
+  selector   : 'app-home',
+  standalone : true,
+  imports    : [
+    ButtonModule,AvatarModule 
+  ],
   templateUrl : './home.component.html',
   styleUrl    : './home.component.scss'
 })
 export class HomeComponent {
+  mockDataAvatars: AvatarInterface[] = [
+    {
+      path  : 'assets/icons/icon1.jpg',
+      label : 'M',
+      size  : 'large',
+      style : { 'background-color': '#ece9fc' },
+      shape : 'circle'
+    },
+    {
+      path  : 'assets/icons/icon2.jpg',
+      label : 'S',
+      size  : 'large',
+      style : { 'background-color': '#dee9fc' },
+      shape : 'circle'
+    }
+  ]
 
+  taskStatus: TaskStatus[] = ['Ready','On Progress','Needs Review','Done'];
+
+  TaskData: {status:TaskStatus}[] = [
+    {
+      status:'Done'
+    },
+    {
+      status:'Ready'
+    }
+  ]
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,20 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin';
 
 export default {
   content : [ './src/**/*.{html,ts}', ],
   theme   : {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase , theme  }) {
+      addBase({
+        h1 : { fontSize: theme('fontSize.4xl') },
+        h2 : { fontSize: theme('fontSize.3xl') },
+        h3 : { fontSize: theme('fontSize.2xl') },
+        h4 : { fontSize: theme('fontSize.xl') },
+      })
+    })
+  ],
 } satisfies Config
 
