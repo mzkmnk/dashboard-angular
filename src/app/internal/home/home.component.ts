c61a4d9a1af10d47f9d0151496c03aa5527151c1
mizuki <114119396+mzkmnk@users.noreply.github.com>
2024-09-15 12:42:57 +0900

    add:primeng && tailwind (#1)

--- src/app/internal/home/home.component.ts
+++ src/app/internal/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  selector    : 'app-home',
+  standalone  : true,
+  imports     : [ ButtonModule ],
+  templateUrl : './home.component.html',
+  styleUrl    : './home.component.scss'
 })
 export class HomeComponent {
 
