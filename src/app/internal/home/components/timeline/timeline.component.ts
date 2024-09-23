ebf10e551c633445e994eeef45fa595008f7047d
mzkmnk <sma0729mex@gmail.com>
2024-09-23 22:46:08 +0900

    wip:timeline追加中

--- src/app/internal/home/components/timeline/timeline.component.ts
+++ src/app/internal/home/components/timeline/timeline.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { DatePipe, KeyValuePipe } from '@angular/common';
+import {Component, inject } from '@angular/core';
+import { ChipModule } from 'primeng/chip';
+import { DividerModule } from 'primeng/divider';
+import { TagModule } from 'primeng/tag';
+
+import { HomeSignalStore } from '../../../stores/home/home.signal-store';
+
+export type TTaskPosition = {
+  top   : string,
+  left  : string,
+  width : string,
+}
 
 @Component({
-  selector    : 'app-timeline',
-  standalone  : true,
-  imports     : [],
+  selector   : 'app-timeline',
+  standalone : true,
+  imports    : [
+    KeyValuePipe,
+    DatePipe,
+    TagModule,
+    DividerModule,
+    ChipModule
+  ],
   templateUrl : './timeline.component.html',
   styleUrl    : './timeline.component.scss'
 })
 export class TimelineComponent {
+  private readonly homeSignalStore = inject(HomeSignalStore);
+  
+  $tasks = this.homeSignalStore.tasks;
 
+  $user = this.homeSignalStore.user;
+
+  /**
+   * ほんとは引数にタスクを取得してstartDayとendDayなどを計算する
+   */
+  calcTaskPostion = ():TTaskPosition => {
+    // w-24 : 6rem; /* 96px */ + border 71らしい
+    // 計算方法(beta)
+    // x =  71*(startDay-1)
+    // startDay = 1
+    // endDay = 5
+    // width = (71 + (0.25*(endDay-startDay))) * (endDay-startDay)
+    const startDay = 2;
+    const endDay = 5 + 1;
+    const adjust = 10;
+    return {
+      top   : '2.5px',
+      left  : 71*(startDay-1)+adjust + 'px',
+      width : (71 + (0.25*(endDay-startDay))) * (endDay-startDay)-adjust*2 + 'px',
+    }
+  }
+
+  /** 月のその月に対する日付を格納する */
+  calendar: {[key in number]:Date[]} = {
+    1 : new Array(31).fill(0).map((_,i) => new Date(2024,0,i+1)),
+    2 : new Array(29).fill(0).map((_,i) => new Date(2024,1,i+1)),
+  };
+
+  /**
+   * タスクの開始日とその日付が一致するかを判定する。
+   * @param date 
+   * @param taskDate 
+   * @returns 
+   */
+  isStartDate = (date:Date,taskDate:Date):boolean => {
+    return date.getTime() === taskDate.getTime()
+  }
 }
